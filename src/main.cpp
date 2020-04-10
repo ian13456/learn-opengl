b3a6c9264b18469794251e1dcbb62f53af56aaa2
Ian Huang <21ianh1@students.tas.tw>
2020-04-10 12:37:43 +0800

    feat: new build system

--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,77 @@
-#include <iostream>
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
 int main() {
-  // Initialize GLFW
-  if (!glfwInit()) {
-    std::cout << "GLFW Initialization failed!" << std::endl;
-    glfwTerminate();
-    return 1;
-  }
+    // Initialize GLFW
+    if (!glfwInit()) {
+        std::cout << "GLFW Initialization failed!" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
 
-  // Setup GLFW window properties
-  // OpenGL version
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  // Core Profile = No Backwards Compatibility
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  // Allow Forward Compatibility
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    // Setup GLFW window properties
+    // OpenGL version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Core Profile = No Backwards Compatibility
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Allow Forward Compatibility
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-  GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Learn OpenGL", NULL, NULL);
-  if (!mainWindow) {
-    std::cout << "GLFW window creation failed!" << std::endl;
-    glfwTerminate();
-    return 1;
-  }
+    GLFWwindow* mainWindow =
+        glfwCreateWindow(WIDTH, HEIGHT, "Learn OpenGL", NULL, NULL);
+    if (!mainWindow) {
+        std::cout << "GLFW window creation failed!" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
 
-  // Get buffer size information
-  int bufferWidth, bufferHeight;
-  glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+    // Get buffer size information
+    int bufferWidth, bufferHeight;
+    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
 
-  // Set context for GLEW to use
-  glfwMakeContextCurrent(mainWindow);
+    // Set context for GLEW to use
+    glfwMakeContextCurrent(mainWindow);
 
-  // Allow modern extension features
-  glewExperimental = GL_TRUE;
+    // Allow modern extension features
+    glewExperimental = GL_TRUE;
 
-  if (glewInit() != GLEW_OK) {
-    std::cout << "GLEW Initialization failed!" << std::endl;
-    glfwDestroyWindow(mainWindow);
-    glfwTerminate();
-    return 1;
-  }
+    if (glewInit() != GLEW_OK) {
+        std::cout << "GLEW Initialization failed!" << std::endl;
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return 1;
+    }
 
-  // Setup viewport size
-  glViewport(0, 0, bufferWidth, bufferHeight);
+    // Setup viewport size
+    glViewport(0, 0, bufferWidth, bufferHeight);
 
-  float test = 0.0f;
-  float inc = 0.01f;
+    float test = 0.0f;
+    float inc  = 0.01f;
 
-  std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
 
-  // Loop until window closed
-  while (!glfwWindowShouldClose(mainWindow)) {
-    // Get + Handle user input events
-    glfwPollEvents();
+    // Loop until window closed
+    while (!glfwWindowShouldClose(mainWindow)) {
+        // Get + Handle user input events
+        glfwPollEvents();
 
-    test += inc;
+        test += inc;
 
-    // Clear window
-    glClearColor(test, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+        // Clear window
+        glClearColor(test, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-    if (test >= 1 || test <= 0) 
-      inc = -inc;
+        if (test >= 1 || test <= 0)
+            inc = -inc;
 
-    glfwSwapBuffers(mainWindow);
-  }
+        glfwSwapBuffers(mainWindow);
+    }
 
-  return 0;
+    return 0;
 }
